18a4fed2aa4e68522e492c7d06948e828df3d722
josther <89234051+josther@users.noreply.github.com>
2022-03-03 14:26:44 +0000

    sakila

--- sakila/vistas/vista_categorias.php
+++ sakila/vistas/vista_categorias.php
@@ -12,7 +12,7 @@ require_once "parte_head.php";
     <div class="container">
         <h3><?php echo $pagina; ?></h3>
         <div class="row">
-            <form class="col-6 ">
+            <form class="col-6 " method="post">
                 <div class="mb-3">
                     <label for="">Nombre</label>
                     <input type="text" name="name" class="form-control">
