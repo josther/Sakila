952340407c71e4806e59cd8e09236eb7820135a6
josther <89234051+josther@users.noreply.github.com>
2022-03-15 15:42:38 +0000

    sakila

--- sakila/categorias.php
+++ sakila/categorias.php
@@ -14,8 +14,24 @@ try {
     echo "</pre>";
     
     //verificar si le da click al boton
-    if (isset($_POST['boton-guardar'])) {
-        echo "guardando...";
+    if (isset($_POST['boton-guardar']) ) {
+    
+
+            // buscar info para editar
+    if ($isser($_GET['boton-guardar'])) {
+        $category_id = $_GET['editar'];
+
+        $query = "SELECT * FROM category WHERE category_id = '$category_id' ";
+
+        $resultado = mysqli_query($conexion, $query);
+
+        if ($resultado) {
+            while ($fila = mysqli_fetch_object($resultado)) {
+                $category_id = $fila->category_id;
+                $name = $fila->name;
+            }
+        }
+    }
         //variable
         $name = $_POST["name"];
         //validaciones
@@ -24,6 +40,13 @@ try {
         }
         //guardar
         $query = "INSERT INTO category (name) VALUES ('$name')";
+
+        // actualizar
+        $id = $_POST['id'] ?? '';
+
+        if ( !empty($id) ) {
+            $query = "UPDATE category SET name = '$name' WHERE category_id = '$id' ";
+        }
 
         $resultado = $conexion->query($query) or die("Error en query");
 
@@ -45,6 +68,8 @@ try {
 } catch(Throwable $ex) {
     $error = $ex->getMessage();
 }
+
+
 
 # incluir vista
 require_once "vistas/vista_categorias.php";
